e7ccca793850f6e7d074594052b94c84e3df1432
yangk <leon.yang@belstar.com.cn>
2020-10-27 22:11:22 +0800

    parse excel

--- test/readExcel.go
+++ test/readExcel.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
+	"strconv"
+	"strings"
 )
 
 type Data struct {
-	FirstClass  string `json:"first_class"`  // 一级
-	SecondClass string `json:"second_class"` // 二级
-	ThirdClass  string `json:"third_class"`  //三级
+	No         string `json:"no"`
+	PrintDate  string `json:"printDate"`
+	AddAt      string `json:"addAt"`
+	ClickCount int    `json:"clickCount"`
+	Color      string `json:"color"`
 }
 
 func ReadExcel(filepath string) ([]Data, error) {
@@ -21,38 +25,59 @@ func ReadExcel(filepath string) ([]Data, error) {
 
 	rows := xlsx.GetRows("Daily Record")
 	var datas []Data
+	var titleMap = make(map[string][3]string)
+	var titleLine = rows[0]
+	for j, title := range titleLine {
+		if title == "时间戳记" || title == "生产日期" {
+			continue
+		}
+		tempArr := GetMacColorAndSize(title)
+		if i := titleMap[tempArr[0]]; i[0] == "" {
+			titleMap[strconv.Itoa(j)] = tempArr
+		}
+	}
+	fmt.Print(titleMap)
+
 	for i, row := range rows {
-		// 去掉第一行，第一行是表头
 		if i == 0 {
 			continue
 		}
 		var data Data
 		for j, colCell := range row {
 			// 排除第一列为Null
-			if j == 0 && colCell == "Null" {
+			if j == 0 || colCell == "Null" {
 				continue
 			}
-			// 第一列即是一级
-			if j == 0 && colCell != "Null" {
-				data.FirstClass = colCell
-			}
-			// 第二列即是二级
-			if j == 1 {
-				data.SecondClass = colCell
-			}
-			// 三级
-			if j == 2 {
-				data.ThirdClass = colCell
-			}
-
+			tempMacMetaInfo := titleMap[strconv.Itoa(j)]
+			data.AddAt = "2020-10-27"
+			data.ClickCount, _ = strconv.Atoi(colCell)
+			data.Color = tempMacMetaInfo[1]
+			data.No = tempMacMetaInfo[0]
+			datas = append(datas, data)
 		}
-		datas = append(datas, data)
 	}
 	return datas, nil
 }
 
+func GetMacColorAndSize(str string) [3]string {
+
+	color := "black"
+	size := "A4"
+	if find := strings.Contains(str, "彩色"); find {
+		color = "color"
+	}
+	if find := strings.Contains(str, "彩印"); find {
+		color = "color"
+	}
+	if find := strings.Contains(str, "A3"); find {
+		size = "A3"
+	}
+
+	return [3]string{str, color, size}
+}
+
 func main() {
-	path := "/Users/yanghaha/WebstormProjects/badge/equ/csv_data/01北京作业中心/11.xlsx"
+	path := "/Users/yanghaha/goproj/src/go-mega/test/11.xlsx"
 	arr, err := ReadExcel(path)
 	if err != nil {
 		fmt.Print(err)
